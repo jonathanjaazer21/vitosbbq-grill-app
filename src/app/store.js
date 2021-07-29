846329a85e28b9704241ef506d40ddd522d808d8
Jonathan Quintana <bojie@c1-link.com>
2021-07-30 01:08:54 +0800

    changing the path of components

--- src/app/store.js
+++ src/app/store.js
@@ -1,11 +1,18 @@
-import { configureStore } from '@reduxjs/toolkit'
-import { TABLE_COMPONENT, NAVIGATION, SCHEDULER_COMPONENT, USER, ORDER_SLIP_COMPONENT, MENU_COMPONENT } from './types'
-import schedulerComponentSlice from 'components/SchedulerComponent/schedulerComponentSlice'
-import sideNav from 'components/sideNav/sideNavSlice'
-import user from 'containers/0.login/loginSlice'
-import branch from 'components/Table/tableSlice'
-import orderSlip from 'components/SchedulerComponent/orderSlip/orderSlipSlice'
-import menuSlice from 'components/sideNav/2.menu/menuSlice'
+import { configureStore } from "@reduxjs/toolkit"
+import {
+  TABLE_COMPONENT,
+  NAVIGATION,
+  SCHEDULER_COMPONENT,
+  USER,
+  ORDER_SLIP_COMPONENT,
+  MENU_COMPONENT,
+} from "./types"
+import schedulerComponentSlice from "../components/SchedulerComponent/schedulerComponentSlice"
+import sideNav from "../components/sideNav/sideNavSlice"
+import user from "../containers/0.login/loginSlice"
+import branch from "../components/Table/tableSlice"
+import orderSlip from "../components/SchedulerComponent/orderSlip/orderSlipSlice"
+import menuSlice from "../components/sideNav/2.menu/menuSlice"
 export default configureStore({
   reducer: {
     [SCHEDULER_COMPONENT]: schedulerComponentSlice,
@@ -13,6 +20,6 @@ export default configureStore({
     [NAVIGATION]: sideNav,
     [TABLE_COMPONENT]: branch,
     [ORDER_SLIP_COMPONENT]: orderSlip,
-    [MENU_COMPONENT]: menuSlice
-  }
+    [MENU_COMPONENT]: menuSlice,
+  },
 })
