b681fc8c952375ef449849bfad918a6f598fd94d
Jonathan Jaazer Quintana <jonathanjaazer21@gmail.com>
2021-09-29 15:13:59 +0800

    done daily summary

--- src/components/features/AnalyticsTransaction/TableDailySummaryContent.js
+++ src/components/features/AnalyticsTransaction/TableDailySummaryContent.js
@@ -1,14 +1,20 @@
-import React from "react"
+import React, { useEffect } from "react"
 import { Space, Table } from "antd"
 import { VerticalAutoScroll } from "Restructured/Components/Features/PaymentTransactionTable/styles"
 import { DATE_START } from "Restructured/Constants/schedules"
 import { AMOUNT_PAID } from "components/PaymentDetails/types"
+import useDailySummary from "./hookDailySummary"
 
-function TableDailySummaryContent() {
+function TableDailySummaryContent({ filteredData = [], dateList = [] }) {
+  const [data, handleData, grandTotal] = useDailySummary()
+
+  useEffect(() => {
+    handleData(filteredData, dateList)
+  }, [filteredData, dateList])
   return (
     <VerticalAutoScroll>
       <Table
-        dataSource={[]}
+        dataSource={[...data, ...grandTotal]}
         pagination={false}
         size="small"
         columns={[
@@ -17,6 +23,7 @@ function TableDailySummaryContent() {
             title: "Amount Paid",
             dataIndex: AMOUNT_PAID,
             key: AMOUNT_PAID,
+            align: "right",
           },
         ]}
       />
