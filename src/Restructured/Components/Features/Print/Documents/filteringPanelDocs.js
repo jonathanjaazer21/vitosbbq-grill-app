ba60842d581d171efb4d4dca608d83e7c693bef9
Jonathan Jaazer Quintana <jonathanjaazer21@gmail.com>
2021-10-05 09:07:06 +0800

    done filtering panel print

--- src/Restructured/Components/Features/Print/Documents/filteringPanelDocs.js
+++ src/Restructured/Components/Features/Print/Documents/filteringPanelDocs.js
@@ -3,6 +3,7 @@ import { useEffect } from "react"
 import { CODE, DESCRIPTION, QUANTITY } from "Restructured/Constants/products"
 import { DATE_END, DATE_START } from "Restructured/Constants/schedules"
 import { Grid } from "Restructured/Styles"
+import { Space } from "antd"
 import FilteringPanelMethods from "../../FilteringPanel/Controllers/FilteringPanelMethods"
 import PrintMethods from "../Controllers/PrintMethods"
 
@@ -40,61 +41,113 @@ function FilteringPanelDocs(props) {
     setPrintDetailsPerSched(_detailsPerSched)
   }
   return (
-    <Grid padding="3rem">
-      <Grid>
-        <h3>VITOS BBQ</h3>
-      </Grid>
-      <Grid>
-        <h3>ORDER LIST FORM</h3>
-      </Grid>
-      <Grid>
-        <div>DETAILS PER PRODUCT</div>
-        <table>
-          <tr>
-            <th>Date</th>
-            <th>Slot</th>
-            <th>Code</th>
-            <th>Product</th>
-            <th style={{ display: "flex", justifyContent: "flex-end" }}>Qty</th>
-          </tr>
-          {printDetailsPerSched.map((data) => {
-            return (
-              <tr>
-                <td>{data.date}</td>
-                <td>{`${data[DATE_START]} - ${data[DATE_END]}`}</td>
-                <td>{data[CODE]}</td>
-                <td>{data[DESCRIPTION]}</td>
-                <td style={{ display: "flex", justifyContent: "flex-end" }}>
-                  {data[QUANTITY]}
-                </td>
-              </tr>
-            )
-          })}
-        </table>
-      </Grid>
-      <br />
-      <Grid>
-        <div>SUMMARY PER PRODUCT</div>
-        <table>
-          <tr>
-            <th>Code</th>
-            <th>Product</th>
-            <th style={{ display: "flex", justifyContent: "flex-end" }}>Qty</th>
-          </tr>
-          {summary.map((data) => {
-            return (
-              <tr>
-                <td>{data[CODE]}</td>
-                <td>{data[DESCRIPTION]}</td>
-                <td style={{ display: "flex", justifyContent: "flex-end" }}>
-                  {data[QUANTITY]}
-                </td>
-              </tr>
-            )
-          })}
-        </table>
-      </Grid>
-    </Grid>
+    <Space direction="vertical" style={{ width: "100%" }}>
+      <span>ORDER LIST</span>
+      <table style={{ fontSize: "8px", width: "100%" }}>
+        <tr style={{ fontWeight: 700, backgroundColor: "#999" }}>
+          <th>DATE</th>
+          <th>SLOT</th>
+          <th>CODE</th>
+          <th>PRODUCT</th>
+          <th>QUANTITY</th>
+        </tr>
+        {printDetailsPerSched.map((data, index) => {
+          return (
+            <tr
+              style={
+                index % 2 === 0
+                  ? { backgroundColor: "white" }
+                  : { backgroundColor: "#999" }
+              }
+            >
+              <td>{data.date}</td>
+              <td>{`${data[DATE_START]} - ${data[DATE_END]}`}</td>
+              <td>{data[CODE]}</td>
+              <td>{data[DESCRIPTION]}</td>
+              <td>{data[QUANTITY]}</td>
+            </tr>
+          )
+        })}
+      </table>
+      <span>SUMMARY PER PRODUCT</span>
+      <table style={{ fontSize: "8px", width: "30%" }}>
+        <tr style={{ fontWeight: 700, backgroundColor: "#999" }}>
+          <th>CODE</th>
+          <th>PRODUCT</th>
+          <th>QUANTITY</th>
+        </tr>
+        {summary.map((data, index) => {
+          return (
+            <tr
+              style={
+                index % 2 === 0
+                  ? { backgroundColor: "white" }
+                  : { backgroundColor: "#999" }
+              }
+            >
+              <td>{data[CODE]}</td>
+              <td>{data[DESCRIPTION]}</td>
+              <td>{data[QUANTITY]}</td>
+            </tr>
+          )
+        })}
+      </table>
+    </Space>
+    // <Grid padding="3rem">
+    //   <Grid>
+    //     <h3>VITOS BBQ</h3>
+    //   </Grid>
+    //   <Grid>
+    //     <h3>ORDER LIST FORM</h3>
+    //   </Grid>
+    //   <Grid>
+    //     <div>DETAILS PER PRODUCT</div>
+    //     <table>
+    //       <tr>
+    //         <th>Date</th>
+    //         <th>Slot</th>
+    //         <th>Code</th>
+    //         <th>Product</th>
+    //         <th style={{ display: "flex", justifyContent: "flex-end" }}>Qty</th>
+    //       </tr>
+    //       {printDetailsPerSched.map((data) => {
+    //         return (
+    //           <tr>
+    //             <td>{data.date}</td>
+    //             <td>{`${data[DATE_START]} - ${data[DATE_END]}`}</td>
+    //             <td>{data[CODE]}</td>
+    //             <td>{data[DESCRIPTION]}</td>
+    //             <td style={{ display: "flex", justifyContent: "flex-end" }}>
+    //               {data[QUANTITY]}
+    //             </td>
+    //           </tr>
+    //         )
+    //       })}
+    //     </table>
+    //   </Grid>
+    //   <br />
+    //   <Grid>
+    //     <div>SUMMARY PER PRODUCT</div>
+    //     <table>
+    //       <tr>
+    //         <th>Code</th>
+    //         <th>Product</th>
+    //         <th style={{ display: "flex", justifyContent: "flex-end" }}>Qty</th>
+    //       </tr>
+    //       {summary.map((data) => {
+    //         return (
+    //           <tr>
+    //             <td>{data[CODE]}</td>
+    //             <td>{data[DESCRIPTION]}</td>
+    //             <td style={{ display: "flex", justifyContent: "flex-end" }}>
+    //               {data[QUANTITY]}
+    //             </td>
+    //           </tr>
+    //         )
+    //       })}
+    //     </table>
+    //   </Grid>
+    // </Grid>
   )
 }
 
